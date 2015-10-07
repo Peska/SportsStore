7479f1e9524cb7c27b8a4f801e4d3ab8340285eb
Peska <peska_jr@o2.pl>
2015-10-07 21:36:26 +0200

    Preparing Ninject Resolver

--- SportsStore.WebUI/App_Start/NinjectWebCommon.cs
+++ SportsStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -3,15 +3,17 @@
 
 namespace SportsStore.WebUI.App_Start
 {
-    using System;
-    using System.Web;
+	using System;
+	using System.Web;
 
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+	using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
-    using Ninject;
-    using Ninject.Web.Common;
+	using Ninject;
+	using Ninject.Web.Common;
+	using System.Web.Mvc;
+	using Infrastructure;
 
-    public static class NinjectWebCommon 
+	public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
@@ -61,6 +63,7 @@ namespace SportsStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+			DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }        
     }
 }
---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Ninject;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+	public class NinjectDependencyResolver : IDependencyResolver
+	{
+		private IKernel kernel;
+
+		public NinjectDependencyResolver(IKernel kernelParam)
+		{
+			kernel = kernelParam;
+			AddBindings();
+		}
+
+		private void AddBindings()
+		{
+			// TODO
+		}
+
+		public object GetService(Type serviceType)
+		{
+			return kernel.TryGet(serviceType);
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return kernel.GetAll(serviceType);
+		}
+	}
+}
